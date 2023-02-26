7f3d337d1dcc55618df8128832b6ff179d342c9c
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-02-26 22:37:33 +0530

    minor changes

--- devcom/src/components/HomePage/HomePage.js
+++ devcom/src/components/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import styles from './HomePage.module.css';
 const HomePage = () => (
   <div className={styles.HomePage}>
     <div className={styles.cover}>
-       HomePage Component
+      <div  className={styles.coverp}>Digitalizing  the<br/>Future <br/> Ahead </div>
     </div>
     <div className={styles.empty1}></div>
   </div>
