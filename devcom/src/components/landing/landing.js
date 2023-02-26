7383f48138de0b5395d53c1170a9f39b783909f2
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2023-02-26 22:21:54 +0530

    landing page

--- devcom/src/components/landing/landing.js
+++ devcom/src/components/landing/landing.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './landing.module.css';
+import { useNavigate } from 'react-router-dom';
+
+var wordlist = ['Ideate.', 'Design.', 'Build.', 'Create.', 'Develop.', 'DevCom.']
+function gothroughwords(){
+  for (let i = 0; i< wordlist.length; i++) {
+  const val = wordlist[i];
+  return val
+  }
+}
+
 
 const Landing = () => (
   <>
   <div className={styles.Landing}>
-    <div className={styles.center_text}></div>
-  </div>
+    <div className={styles.center_text}>
+      {gothroughwords()}
+    </div>
+   </div>
+  <div className={styles.circle}/>
+  {setTimeout(() => {
+    navigate('/home')
+  }, 3000)}
   </>
 );
 
