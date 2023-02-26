3857066cc5924802dd56d6917e9be21925de1a90
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-02-26 19:53:31 +0530

    footer, font

--- devcom/src/App.js
+++ devcom/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Landing from './components/landing/landing';
 import Footer from './components/Footer/Footer';
+import "./fonts/Lato/Lato-Regular.ttf";
+import "./fonts/Lato/Lato-Black.ttf";
 
 function App() {
   return (
--- devcom/src/components/Footer/Footer.js
+++ devcom/src/components/Footer/Footer.js
@@ -1,12 +1,27 @@
 import React from "react";
 import styles from './Footer.module.css';
+import logo from '../../assets/logo_white.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import{faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => {
 return (
-<div>
-<div>devcom icon</div>
-<div>bar</div>
-<div>Contact us</div>
+<div className={styles.Footer}>
+    <div className={styles.icon}><div className={styles.img}><img height='60' src={logo} /></div></div>
+    <div className={styles.bar}><div className={styles.Btop}></div><div className={styles.Bbottom}></div></div>
+    <div className={styles.container}>
+        <div className={styles.Ctop}>
+            <p>Contact us</p>
+        </div>
+        <div className={styles.Cbottom}>
+            <FontAwesomeIcon icon={faInstagram} />&nbsp;
+            <FontAwesomeIcon icon={faFacebook} />&nbsp;
+            <FontAwesomeIcon icon={faLinkedin} />&nbsp;
+            <FontAwesomeIcon icon={faTwitter} />&nbsp;
+            <FontAwesomeIcon icon={faEnvelope} />
+        </div>
+    </div>
 </div>
 );
 };
