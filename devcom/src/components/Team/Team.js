e8df1336863907fa723375068960bcb8c8c8f781
SanskarGosavi2003 <210100132@iitb.ac.in>
2023-02-27 06:51:30 +0530

    Photu chapvai sabki bhai ne

--- devcom/src/components/Team/Team.js
+++ devcom/src/components/Team/Team.js
@@ -2,7 +2,21 @@ import React from 'react';
 import useFetch from "./useFetch";
 import PropTypes from 'prop-types';
 import styles from './Team.module.css';
-import img1 from './DC_logo.png';
+import img1 from '../../assets/avatars/DevDesai.jpg'
+import img2 from '../../assets/avatars/Shruti Singh - Shruti.jpg'
+import img3 from '../../assets/avatars/Palash Mittal.jpg'
+import img4 from '../../assets/avatars/Harsh Jha.png'
+import img5 from '../../assets/avatars/Adithya Krishna K.jpg'
+import img6 from '../../assets/avatars/Ashwin Ram.jpg'
+import img7 from '../../assets/avatars/ayush kapoor.jpg'
+import img8 from '../../assets/avatars/DevDesai.jpg'
+import img9 from '../../assets/avatars/DevDesai.jpg'
+import img10 from '../../assets/avatars/DevDesai.jpg'
+import img11 from '../../assets/avatars/DevDesai.jpg'
+import img12 from '../../assets/avatars/DevDesai.jpg'
+import img13 from '../../assets/avatars/DevDesai.jpg'
+import img14 from '../../assets/avatars/DevDesai.jpg'
+import img15 from '../../assets/avatars/DevDesai.jpg'
 
 // async function jsonChecker(){
 //   const response_ocs = await fetch('http://localhost:7000/OCs');
@@ -17,15 +31,15 @@ const db = {
       {
           "id":1,
           "name": "Dev Moxaj Desai",
-          "img": " ",
-          "fb": "",
+          "img":img1,
+          "fb": "https://m.facebook.com/dev.desai.77920?ref=bookmarks",
           "insta": "",
           "linkedIn": "" 
       },
       {
           "id":2,
           "name": "Shruti Singh",
-          "img": " ",
+          "img": img2,
           "fb": "",
           "insta": "",
           "linkedIn": ""
@@ -34,34 +48,17 @@ const db = {
   ],
   "PLs":[
       {
-          "id":1,
-          "name": "Palash Mittal",
-          "img": " ",
-          "fb": "",
-          "insta": "",
-          "linkedIn": "" 
-      },
-      {
-          "id":2,
-          "name": "harsh Jha",
-          "img": " ",
-          "fb": "",
-          "insta": "",
-          "linkedIn": ""
-                  
-      },
-      {
           "id":3,
-          "name": "Adithya Krishna",
-          "img": " ",
+          "name": "Palash Mittal",
+          "img": img3,
           "fb": "",
           "insta": "",
           "linkedIn": "" 
       },
       {
           "id":4,
-          "name": "Ashwin Ramchandran",
-          "img": " ",
+          "name": "harsh Jha",
+          "img": img4,
           "fb": "",
           "insta": "",
           "linkedIn": ""
@@ -69,8 +66,25 @@ const db = {
       },
       {
           "id":5,
+          "name": "Adithya Krishna",
+          "img": img5,
+          "fb": "",
+          "insta": "",
+          "linkedIn": "" 
+      },
+      {
+          "id":6,
+          "name": "Ashwin Ramchandran",
+          "img": img6,
+          "fb": "",
+          "insta": "",
+          "linkedIn": ""
+                  
+      },
+      {
+          "id":7,
           "name": "Ayush Kapoor",
-          "img": " ",
+          "img": img7,
           "fb": "",
           "insta": "",
           "linkedIn": "" 
@@ -84,7 +98,12 @@ const Team = () => {
     <div className={styles.Team}>
     <div className={styles.OCs}>{db.OCs.map((OC) => 
       <li key={OC.id}>
-        <img src={img1} height='55'/>
+        <img src={OC.img} height='55'/>
+      </li>
+    )}</div>
+    <div className={styles.PLs}>{db.PLs.map((PL) => 
+      <li key={PL.id}>
+        <img src={PL.img} height='55'/>
       </li>
     )}</div>
   </div>
